43a4a66686604e8a433a1bf8cf521e60c6b2010d
Krystian Kalecki <krystian.kalecki@o2.pl>
2021-04-14 16:16:18 +0200

    first commit

---
+++ src/main/java/com/project/TaskListsAPI/TaskListsApiApplication.java
@@ -0,0 +1,13 @@
+package com.project.TaskListsAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskListsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskListsApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/TaskListsAPI/TaskListsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.TaskListsAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskListsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
