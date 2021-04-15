2c40a9efa0325e10c03b984766c5ee46ede8f678
Krystian Kalecki <krystian.kalecki@o2.pl>
2021-04-15 12:39:29 +0200

    creating database and security

--- src/main/java/com/project/TaskListsAPI/controller/RestControl.java
+++ src/main/java/com/project/TaskListsAPI/controller/RestControl.java
@@ -1,2 +1,27 @@
-package com.project.TaskListsAPI.controller;public class RestController {
+package com.project.TaskListsAPI.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RestControl {
+
+    @GetMapping("/")
+    public String welcomeEveryone()
+    {
+        return "<h1>Welcome Everyone</h1>";
+    }
+    @GetMapping("/user")
+    public String welcomeUser()
+    {
+        return "<h1>Welcome Everyone Logged in</h1>";
+    }
+    @GetMapping("/admin")
+    public String welcomeAdmin()
+    {
+        return "<h1>Welcome Admin!</h1>";
+    }
+
+
 }
